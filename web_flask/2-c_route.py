14b28a83b40d3bf068ba36dd1c4077e89935092f
BradBrown6621 <6621@holbertonstudents.com>
2024-07-09 23:30:04 -0500

    2-c_route.py: Demostrates ability to create routing options using user input

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Hello World! script that has routing for the following:
+    '/',
+    '/hbnb'
+    '/c/<usertext>'
+"""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Hello, HBNB!
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    HBNB
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    C {}
+    """
+    return "C {}".format(escape(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
